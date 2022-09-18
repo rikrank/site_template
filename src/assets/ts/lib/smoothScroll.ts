df6cc9d47c6868b08c59edc0652f4754dd5275f1
rikrank <morishita-r@tam-tam.co.jp>
2022-09-18 14:47:51 +0900

    Feat：CSS 環境作成

--- src/assets/ts/lib/smoothScroll.ts
+++
@@ -1,8 +0,0 @@
-import SmoothScroll from "smooth-scroll";
-const smoothScroll = new SmoothScroll("a[href*='#']", {
-  speed: 1000,
-  speedAsDuration: true,
-  easing: "easeInOutQuint",
-  // offset: 80,
-});
-export { smoothScroll };
